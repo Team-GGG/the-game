1511992b4117ed29daa600a6a98111a77fc71ff5
Tarek Alam Bhuiyan <tarekalambhuiyan@gmail.com>
2026-07-18 22:42:03 +0600

    BufferFix boss health check changed intentionally

--- game.c
+++ game.c
@@ -4462,6 +4462,7 @@ int main() {
         ScoreWrite(&board);
 
         // RESETING EVERYTHING
+        buffer[0] = '\0';
         menu = MAIN_MENU;
         player_state.player->y = 65 * 32;
 
@@ -4541,7 +4542,7 @@ int main() {
       // if (IsKeyPressed(KEY_Y)) {
       // menu = SCOREBOARD_MENU;
       //}
-      if (boss.hp <= 0) {
+      if (boss.hp <= -0.05) {
         PlaySound(sound_win);
         menu = INPUT_MENU;
       }
